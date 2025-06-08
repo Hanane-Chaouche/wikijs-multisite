3931353a3658b8ba8a3b0e1dc19d594a1337d619
Hanane Chaouche <6169778@bdeb.qc.ca>
2025-06-08 16:25:07 -0400

    Update webhook.py

--- paypal-webhook/webhook.py
+++ paypal-webhook/webhook.py
@@ -1,1 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/paypal/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print(" Webhook PayPal reçu :", data)
+
+    if data.get("event_type") == "PAYMENT.SALE.COMPLETED":
+        print("Paiement confirmé !")
+        # je peux ajouter ici des actions (ajouter utilisateur, envoyer un email, etc.)
+
+    return '', 200
 
