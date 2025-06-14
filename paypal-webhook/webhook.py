85aaa4f7347438522743a789a10558c29d90603a
Hanane Chaouche <6169778@bdeb.qc.ca>
2025-06-14 10:57:25 -0400

    Update webhook.py

--- paypal-webhook/webhook.py
+++ paypal-webhook/webhook.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
-@app.route('/paypal/webhook', methods=['POST'])
+
+@app.route('/paypal/webhook', methods=['GET', 'POST'])
 def webhook():
+    if request.method == 'GET':
+        return "Webhook PayPal actif (GET)", 200
+
     data = request.json
     with open("/tmp/paypal_log.txt", "a") as f:
         f.write(str(data) + "\n")
     print("Webhook PayPal (enseignant) reçu :", data)
+
     if data.get("event_type") == "PAYMENT.SALE.COMPLETED":
-        print("Paiement réussi ")
+        print("✅ Paiement réussi !")
+
     return jsonify({'status': 'OK'}), 200
